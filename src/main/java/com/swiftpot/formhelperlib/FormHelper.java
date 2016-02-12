937e78b126a9ad27a9f0d0a33f0789ba72da40fd
AcePRbk[Rodney] <rodney@smsgh.com>
2016-02-12 19:01:47 +0000

    added void method as parameter,getting InstantiationException,handle in next iteration

--- src/main/java/com/swiftpot/formhelperlib/FormHelper.java
+++ src/main/java/com/swiftpot/formhelperlib/FormHelper.java
@@ -3,29 +3,58 @@ package com.swiftpot.formhelperlib;
 import android.view.View;
 import android.widget.EditText;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 /**
  * Created by Rodney on 12-Feb-16.
  */
 public abstract class FormHelper implements View.OnClickListener{
 
 
-    public FormHelper(EditText [] editTexts){
-    validateEmptyEditTexts(editTexts);
+    public FormHelper(EditText [] editTexts,Method nonStaticVoidMethod){
+        this.editTexts = editTexts;
+        this.nonStaticVoidMethod = nonStaticVoidMethod;
     }
+    EditText [] editTexts;
+    Method nonStaticVoidMethod;
 
     @Override
     public void onClick(View view) {
-
-    }
-
-    protected void validateEmptyEditTexts(EditText [] editTexts){
-
-        for (EditText editText: editTexts) {
-            if(editText.getText().toString().trim().isEmpty()){
-                editText.setError("Field Cannot Be Empty");
-                return;
-            }
-
+    if(isEditTextEmpty(editTexts) == false){
+        try {
+            Class c = Class.forName(getClass().getName());
+            Object t = c.newInstance();
+            nonStaticVoidMethod.invoke(t);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (InstantiationException e){
+            e.printStackTrace();
         }
+    }else{
+        //do nothing as errorText will be set On editText
     }
+    }
+
+     final protected boolean isEditTextEmpty(EditText [] editTexts) {
+         boolean status = true;
+         for (EditText editText : editTexts) {
+             if (editText.getText().toString().trim().isEmpty()) {
+                 editText.setError("Field Cannot Be Empty");
+                 status = true;
+                 break;
+             } else {
+                 //not empty,continue
+                 status =false;
+             }
+
+         }
+
+         return status;
+     }
 }
